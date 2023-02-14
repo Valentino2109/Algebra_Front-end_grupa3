064e4e8f0b1df275ed10396ae0224c3764d1169d
Valentino Benković <vbenkovic92@gmail.com>
2023-02-14 21:05:14 +0100

    doma1

---
+++ React/Domace zadace/doma1/doma-jedan/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello everyone</h3>
+      <p>This is just a test...</p>
+    </div>
+  );
+}
+
+export default App;
